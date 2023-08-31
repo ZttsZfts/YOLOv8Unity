438e7c34b34922142235e6e7aa158aaa7b941116
ZttsZfts <zttszfts@gmail.com>
2023-08-31 11:40:17 +0800

    新增描绘推理区域，但存在y轴镜像问题，修复严重的内存溢出问题

--- Assets/Scripts/PlayCamera.cs
+++ Assets/Scripts/PlayCamera.cs
@@ -6,6 +6,7 @@ using OpenCVForUnity.ImgprocModule;
 using OpenCVForUnity.UnityUtils;
 using Unity.Barracuda;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
 using Tensor = Unity.Barracuda.Tensor;
@@ -16,6 +17,9 @@ public class PlayCamera : MonoBehaviour
     public RawImage rawImage;
     public float threshold = 0.7f;
 
+    public Transform wireframeCarrier;
+    public Transform wireframe;
+
     private WebCamTexture _webCamTexture;
     private IWorker _worker;
 
@@ -25,25 +29,54 @@ public class PlayCamera : MonoBehaviour
         var webCamDevices = WebCamTexture.devices;
         _webCamTexture = new WebCamTexture(webCamDevices[0].name, 1024, 768, 30);
         _webCamTexture.Play();
+        rawImage.texture = _webCamTexture;
+        
+        StartCoroutine(Illation(AfterTreatment));
+    }
 
-        StartCoroutine(Illation());
+    private void AfterTreatment(List<YoloResult> yoloResults)
+    {
+        for (var i = 0; i < wireframeCarrier.childCount; i++) Destroy(wireframeCarrier.GetChild(i).gameObject);
+
+        foreach (var yoloResult in yoloResults)
+        {
+            var label = yoloResult.Label;
+            var instantiate = Instantiate(wireframe, wireframeCarrier);
+            instantiate.GetComponentsInChildren<Text>()[0].text = label.ToString();
+            instantiate.GetComponent<RectTransform>().sizeDelta =
+                new Vector2(
+                    yoloResult.Rect.Width * (1024f / 640f) *
+                    (wireframeCarrier.GetComponent<RectTransform>().rect.width /
+                     1024f),
+                    yoloResult.Rect.Height * (768f / 640f) *
+                    (wireframeCarrier.GetComponent<RectTransform>().rect.height /
+                     768f)
+                );
+            instantiate.GetComponent<RectTransform>().position =
+                new Vector3(
+                    yoloResult.Rect.X * (1024f / 640f) *
+                    (wireframeCarrier.GetComponent<RectTransform>().rect.width / 1024f),
+                    yoloResult.Rect.Y * (768f / 640f) *
+                    (wireframeCarrier.GetComponent<RectTransform>().rect.height /
+                     768f)
+                );
+        }
     }
 
 
-    private IEnumerator Illation()
+    private IEnumerator Illation(UnityAction<List<YoloResult>> process)
     {
         while (true)
         {
-            var cameraInput = new Mat(_webCamTexture.height, _webCamTexture.width, CvType.CV_8UC3);
+            using var cameraInput = new Mat(_webCamTexture.height, _webCamTexture.width, CvType.CV_8UC3);
             Utils.webCamTextureToMat(_webCamTexture, cameraInput);
-            var cameraInputZoom = new Mat(640, 640, CvType.CV_8UC3);
+            using var cameraInputZoom = new Mat(640, 640, CvType.CV_8UC3);
             Imgproc.resize(cameraInput, cameraInputZoom, new Size(cameraInputZoom.cols(), cameraInputZoom.rows()));
             var cameraInputTexture2D = Mat2Texture2D(cameraInputZoom);
             yield return new WaitForEndOfFrame();
-            rawImage.texture = cameraInputTexture2D;
-            var inputTensor = new Tensor(cameraInputTexture2D, 3);
-
-            var outputTensor = _worker.Execute(inputTensor).PeekOutput();
+            using var inputTensor = new Tensor(cameraInputTexture2D, 3);
+            Destroy(cameraInputTexture2D);
+            using var outputTensor = _worker.Execute(inputTensor).PeekOutput();
 
             var yoloResult = new List<YoloResult>();
             var labelNumber = Enum.GetNames(typeof(YoloLabels)).Length;
@@ -72,7 +105,8 @@ public class PlayCamera : MonoBehaviour
                 if (confidenceMax >= threshold) yoloResult.Add(yoloItem);
             }
 
-            yield return 0;
+            process(yoloResult);
+            yield return new WaitForSeconds(0.2f);
         }
     }
 
